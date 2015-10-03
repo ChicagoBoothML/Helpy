994adec54b33bb9daf2c0e30c5b259b2600c7606
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-03 11:07:14 -0500

    initial package upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='ChicagoBoothML_Helpy',
+    version='0.0.1',
+    packages=['ChicagoBoothML_Helpy'],
+    url='http://GitHub.com/ChicagoBoothML/Helpy',
+    author='Chicago Booth Machine Learning course team',
+    author_email='MBALearnsToCode@UChicago.edu',
+    description='Python helper functions',
+    long_description='Python helper functions',
+    license='MIT License',
+    install_requires=['Keras', 'SciKit-Learn', 'Theano'],
+    classifiers=[],   # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='Chicago Machine Learning Python')
