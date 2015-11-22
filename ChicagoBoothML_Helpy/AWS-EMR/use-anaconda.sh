8b04d8d3e0ad54e41d18725cd6fcb763ecdcb059
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-11-22 01:57:07 -0600

    update use-anaconda.sh script

--- ChicagoBoothML_Helpy/AWS-EMR/use-anaconda.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/use-anaconda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo chmod 777 /usr/lib/spark/conf/spark-env.sh
-sudo echo "; PYSPARK_PYTHON=/Anaconda/bin/python2.7" >> /usr/lib/spark/conf/spark-env.sh
+sudo echo "; export PYSPARK_PYTHON=/Anaconda/bin/python2.7" >> /usr/lib/spark/conf/spark-env.sh
