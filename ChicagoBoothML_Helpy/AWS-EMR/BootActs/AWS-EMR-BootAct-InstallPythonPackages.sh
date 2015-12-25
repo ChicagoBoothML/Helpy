c68ad81ca88fb5d1098aa30ec69ff86e8a980c62
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-12-25 14:58:17 -0600

    move CUDA & related packages installation to AWS EMR Step script

--- ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallPythonPackages.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallPythonPackages.sh
@@ -11,13 +11,7 @@ mkdir $HOME
 
 export SPARK_HOME=/usr/lib/spark
 
-export CUDA_ROOT=/mnt/cuda-7.5
-mkdir $CUDA_ROOT
-
-export TMPDIR=/mnt/tmp
-mkdir -p $TMPDIR
-
-export HOMEBREW_TEMP=$TMPDIR
+export HOMEBREW_TEMP=/mnt/tmp
 
 export KERNEL_RELEASE=$(uname -r)
 export KERNEL_SOURCE_PATH=/usr/src/kernels/$KERNEL_RELEASE
@@ -89,47 +83,6 @@ sudo ln -s $(which g++) `brew --prefix`/bin/g++-$(g++ -dumpversion |cut -d. -f1,
 sudo ln -s $(which gfortran) `brew --prefix`/bin/gfortran-$(gfortran -dumpversion |cut -d. -f1,2)
 
 
-# change directory to Temp folder to install NVIDIA driver & CUDA toolkit
-cd $TMPDIR
-
-# install NVIDIA driver
-# (ref: http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using_cluster_computing.html#install-nvidia-driver)
-# G2 Instances
-# Product Type: GRID
-# Product Series: GRID Series
-# Product: GRID K520
-# Operating System: Linux 64-bit
-# Recommended/Beta: Recommended/Certified
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/358.16/NVIDIA-Linux-x86_64-358.16.run
-# the following installation fails on non-NVIDIA GPU nodes,
-# hence we turn off the strict error trap temporarily and turn it back on again
-set +e
-sudo sh NVIDIA-Linux-x86_64-358.16.run --silent --kernel-source-path $KERNEL_SOURCE_PATH --tmpdir $TMPDIR
-set -e
-
-# install CUDA toolkit
-wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
-sudo sh cuda_7.5.18_linux.run --silent --driver --toolkit --toolkitpath $CUDA_ROOT --extract $TMPDIR --kernel-source-path $KERNEL_SOURCE_PATH --tmpdir $TMPDIR
-sudo sh cuda-linux64-rel-7.5.18-19867135.run --noprompt --prefix $CUDA_ROOT --tmpdir $TMPDIR
-# add CUDA executables & libraries to Path
-# instructions: Please make sure that
-# -   PATH includes /mnt/cuda-7.5/bin
-# -   LD_LIBRARY_PATH includes /mnt/cuda-7.5/lib64, or,
-# add /mnt/cuda-7.5/lib64 to /etc/ld.so.conf and run ldconfig as root
-export PATH=$PATH:$CUDA_ROOT/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib:$CUDA_ROOT/lib64
-# :/usr/local/cuda/nvvm/libdevice
-echo "$CUDA_ROOT/lib"      > ~/cuda.conf
-echo "$CUDA_ROOT/lib64"   >> ~/cuda.conf
-sudo mv ~/cuda.conf /etc/ld.so.conf.d/
-sudo ldconfig
-sudo ln -s $CUDA_ROOT/bin/nvcc /usr/bin/nvcc
-
-
-# change directory back to Home folder
-cd ~
-
-
 # download PostgreSQL JDBC driver
 curl https://jdbc.postgresql.org/download/postgresql-9.4-1205.jdbc42.jar --output PostgreSQL_JDBC.jar
 
@@ -181,35 +134,12 @@ sudo pip install --upgrade Plotly
 
 # Machine Learning packages
 sudo pip install --upgrade H2O
-# sudo pip install --upgrade Orange   takes long
+sudo pip install --upgrade Orange
 sudo pip install --upgrade SKLearn-Pandas
 sudo pip install --upgrade Sparkit-Learn
 
-# CUDA, Theano & Deep Learning
-git clone --recursive http://git.tiker.net/trees/pycuda.git
-cd pycuda
-sudo python configure.py --cuda-root=$CUDA_ROOT
-# the following installation fails on non-NVIDIA GPU nodes,
-# hence we turn off the strict error trap temporarily and turn it back on again
-set +e
-sudo make install
-set -e
-cd ..
-sudo rm -r pycuda
-
-#   sudo pip install git+https://github.com/cudamat/cudamat.git   installation fails
-
-# the following installation may fail on non-GPU nodes,
-# hence we turn off the strict error trap temporarily and turn it back on again
-set +e
-sudo pip install --upgrade SciKit-CUDA
-set -e
-
-sudo pip install GNumPy
-
+# Theano & Deep Learning
 sudo pip install --upgrade Theano
-sudo pip install --upgrade git+git://github.com/sklearn-theano/sklearn-theano
-
 sudo pip install --upgrade git+git://github.com/mila-udem/fuel.git
 sudo pip install --upgrade git+git://github.com/mila-udem/blocks.git
 sudo pip install --upgrade Chainer
@@ -220,13 +150,6 @@ sudo pip install --upgrade DeepDish
 sudo pip install --upgrade Deepy
 #   sudo pip install --upgrade FANN2   need C FANN
 sudo pip install --upgrade FFnet
-
-# the following installation may fail on non-GPU nodes,
-# hence we turn off the strict error trap temporarily and turn it back on again
-set +e
-sudo pip install --upgrade Hebel
-set -e
-
 sudo pip install --upgrade Keras
 sudo pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 sudo pip install --upgrade Mang
@@ -251,6 +174,7 @@ cd ..
 
 sudo pip install --upgrade PythonBrain
 sudo pip install --upgrade SciKit-NeuralNetwork
+sudo pip install --upgrade git+git://github.com/sklearn-theano/sklearn-theano
 #   sudo pip install --upgrade Synapyse   installation fails
 #   sudo pip install --upgrade Syntaur   not yet available
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
