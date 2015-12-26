ac41c42819e7d6f1e6a6c852320271d3e7d3edc8
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-12-26 11:09:11 -0600

    update LD_LIBRARY_PATH with nvvm/libdevice folder path

--- ChicagoBoothML_Helpy/AWS-EMR/Steps/AWS-EMR-Step-InstallDeepLearningPackages.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/Steps/AWS-EMR-Step-InstallDeepLearningPackages.sh
@@ -36,9 +36,9 @@ sudo sh cuda-linux64-rel-7.5.18-19867135.run --noprompt --prefix $CUDA_ROOT --tm
 # -   PATH includes /mnt/cuda-7.5/bin
 # -   LD_LIBRARY_PATH includes /mnt/cuda-7.5/lib64, or,
 # add /mnt/cuda-7.5/lib64 to /etc/ld.so.conf and run ldconfig as root
-# :/usr/local/cuda/nvvm/libdevice
-echo "$CUDA_ROOT/lib"      > cuda.conf
-echo "$CUDA_ROOT/lib64"   >> cuda.conf
+echo "$CUDA_ROOT/lib"               > cuda.conf
+echo "$CUDA_ROOT/lib64"            >> cuda.conf
+echo "$CUDA_ROOT/nvvm/libdevice"   >> cuda.conf
 sudo mv cuda.conf /etc/ld.so.conf.d/
 sudo ldconfig
 sudo ln -s $CUDA_ROOT/bin/nvcc /usr/bin/nvcc
