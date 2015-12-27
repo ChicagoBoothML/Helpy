a8a33c7fddebbe89cc73c1dd35b5a82de0e09f6c
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-12-27 14:27:35 -0600

    update Keras training monitor

--- ChicagoBoothML_Helpy/KerasTrainingMonitor.py
+++ ChicagoBoothML_Helpy/KerasTrainingMonitor.py
@@ -1,8 +1,9 @@
 from __future__ import division
-import bokeh
+import bokeh.plotting
 import copy
 import keras
 import numpy
+import os
 import Print
 
 
@@ -11,7 +12,7 @@ import Print
 # it also includes a method "plot_learning_curves" that turns on a separate CPU process
 # that plots the Training and Validation learning curves live
 class NeuralNetworkTrainingMonitor(keras.callbacks.Callback):
-    def __init__(self, reporting_freq=False, plot_title='Neural Network Learning Curves'):
+    def __init__(self, reporting_freq=False, plot_title='Neural Network Learning Curves', bokeh_output='notebook'):
         super(keras.callbacks.Callback, self).__init__()
         self.latest_epoch = -1
         self.latest_batch = -1
@@ -27,39 +28,59 @@ class NeuralNetworkTrainingMonitor(keras.callbacks.Callback):
         self.best_model_val_acc = None
         self.reporting_freq = reporting_freq
 
-        bokeh.plotting.output_notebook()
-        self.fig = \
-            bokeh.plotting.figure(
-                title=plot_title,
-                x_axis_label='# of Training Data Batches',
-                y_axis_label='Loss',
-                plot_height=680,
-                plot_width=880)
-        self.fig_train_losses_data_source = \
-            bokeh.models.ColumnDataSource(
-                data=dict(
-                    batches=self.batches,
-                    train_losses=self.train_losses))
-        self.fig.line(
-                x='batches',
-                y='train_losses',
-                source=self.fig_train_losses_data_source,
-                name='TrainLoss',
-                legend='Training Loss')
-        self.fig_val_losses_data_source = \
-            bokeh.models.ColumnDataSource(
-                data=dict(
-                    batches=self.batches,
-                    val_losses=self.val_losses))
-        self.fig.circle(
-            x='batches',
-            y='val_losses',
-            source=self.fig_val_losses_data_source,
-            name='ValidLoss',
-            legend='Validation Loss',
-            color='red')
-        bokeh.plotting.show(self.fig)
+        self.bokeh_output = bokeh_output
+        if bokeh_output == 'notebook':
+            bokeh.plotting.output_notebook(hide_banner=True)
+        elif bokeh_output == 'server':
+            Print.printflush(
+                '\nConnecting to Bokeh Server for live Learning Curves plotting...\n')
+            bokeh.plotting.output_server('')
+            self.bokeh_session = bokeh.plotting.cursession()
 
+        try:
+            self.fig = \
+                bokeh.plotting.figure(
+                    title=plot_title,
+                    x_axis_label='# of Training Data Batches',
+                    y_axis_label='Loss',
+                    plot_height=680,
+                    plot_width=880)
+            self.fig_train_losses_data_source = \
+                bokeh.models.ColumnDataSource(
+                    data=dict(
+                        batches=self.batches,
+                        train_losses=self.train_losses))
+            self.fig.line(
+                    x='batches',
+                    y='train_losses',
+                    source=self.fig_train_losses_data_source,
+                    name='TrainLoss',
+                    legend='Training Loss')
+            self.fig_val_losses_data_source = \
+                bokeh.models.ColumnDataSource(
+                    data=dict(
+                        batches=self.batches,
+                        val_losses=self.val_losses))
+            self.fig.circle(
+                x='batches',
+                y='val_losses',
+                source=self.fig_val_losses_data_source,
+                name='ValidLoss',
+                legend='Validation Loss',
+                color='red')
+            bokeh.plotting.show(self.fig)
+        except:
+            Print.printflush(
+                '\nBokeh Notebook/Server Connection *FAILED!*')
+            Print.printflush(
+                'Please make sure Bokeh package is already installed in Python, and')
+            Print.printflush(
+                'please open a new Command-Line Terminal window\n   (separate from this Terminal window)')
+            Print.printflush(
+                '   and run the following command firs to launch Bokeh Server:')
+            Print.printflush(
+                '       bokeh-server --backend=memory\n')
+            os._exit(0)
 
     def on_train_begin(self, logs={}):
         Print.printflush('\nFFNN Training Progress')
@@ -75,7 +96,8 @@ class NeuralNetworkTrainingMonitor(keras.callbacks.Callback):
         train_acc = logs.get('acc')
         if not train_acc:
             train_acc = logs.get('accuracy')
-        self.approx_train_acc_in_latest_epoch += (train_acc - self.approx_train_acc_in_latest_epoch) / (batch + 1)
+        self.approx_train_acc_in_latest_epoch += \
+            (train_acc - self.approx_train_acc_in_latest_epoch) / (batch + 1)
         self.val_losses.append(logs.get('val_loss', numpy.nan))
         if self.reporting_freq and not(self.latest_batch % self.reporting_freq):
             self.report(batch_in_epoch=batch)
@@ -133,5 +155,14 @@ class NeuralNetworkTrainingMonitor(keras.callbacks.Callback):
                '{:.1f}'.format(100. * self.approx_train_acc_in_latest_epoch),
                val_acc_text), end='\r')
 
-        self.fig_train_losses_data_source.push_notebook()
-        self.fig_val_losses_data_source.push_notebook()
+        if self.bokeh_output == 'notebook':
+            # NOTE: there are currently bugs relared to memory leaks & JavaScript
+            # ref: http://stackoverflow.com/questions/34442414/safely-use-push-notebook-in-bokeh-before-plot-is-shown
+            # ref: https://github.com/bokeh/bokeh/issues/1732
+            # ref: https://github.com/bokeh/bokeh/pull/3370
+            self.fig_train_losses_data_source.push_notebook()
+            self.fig_val_losses_data_source.push_notebook()
+        elif self.bokeh_output == 'server':
+            self.bokeh_session.store_objects(
+                self.fig_train_losses_data_source,
+                self.fig_val_losses_data_source)
