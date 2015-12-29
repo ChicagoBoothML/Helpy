ebbc022aa7c4aa0b15239f2f9aca9e33dd0959fd
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-12-29 15:32:02 -0600

    use sudo to download and modify files in /usr/lib/spark/conf folder

--- ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallBasics.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallBasics.sh
@@ -28,8 +28,8 @@ source .EnvVars
 # modify default Spark Defaults Config (spark-defaults.conf) file
 cd $SPARK_HOME/conf
 sudo mv $SPARK_DEFAULTS_CONF_FILE_NAME ARCHIVED-$SPARK_DEFAULTS_CONF_FILE_NAME
-wget $GITHUB_REPO_RAW_PATH/.config/$SPARK_DEFAULTS_CONF_FILE_NAME
-dos2unix $SPARK_DEFAULTS_CONF_FILE_NAME
+sudo wget $GITHUB_REPO_RAW_PATH/.config/$SPARK_DEFAULTS_CONF_FILE_NAME
+sudo dos2unix $SPARK_DEFAULTS_CONF_FILE_NAME
 
 
 # download additional Yum Repo specs files
