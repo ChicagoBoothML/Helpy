026ca8f4729e305e5d60c2fb800380edfd9bf076
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-12-29 19:50:01 -0600

    move Spark Defaults Conf modification script to separate Step

--- ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallBasics.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-InstallBasics.sh
@@ -25,13 +25,6 @@ dos2unix .EnvVars
 source .EnvVars
 
 
-# modify default Spark Defaults Config (spark-defaults.conf) file
-cd $SPARK_HOME/conf
-sudo mv $SPARK_DEFAULTS_CONF_FILE_NAME ARCHIVED-$SPARK_DEFAULTS_CONF_FILE_NAME
-sudo wget $GITHUB_REPO_RAW_PATH/.config/$SPARK_DEFAULTS_CONF_FILE_NAME
-sudo dos2unix $SPARK_DEFAULTS_CONF_FILE_NAME
-
-
 # download additional Yum Repo specs files
 cd /etc/yum.repos.d
 sudo wget $GITHUB_REPO_RAW_PATH/YumRepos/fedora.repo
