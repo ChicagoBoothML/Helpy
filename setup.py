4ac27f1ac42136be67f2fdc8688e15d20e275211
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-01 15:34:58 -0600

    update to v0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ChicagoBoothML_Helpy',
-    version='0.1.5',
+    version='0.1.6',
     packages=['ChicagoBoothML_Helpy'],
     url='http://GitHub.com/ChicagoBoothML/Helpy',
     author='Chicago Booth Machine Learning course team',
