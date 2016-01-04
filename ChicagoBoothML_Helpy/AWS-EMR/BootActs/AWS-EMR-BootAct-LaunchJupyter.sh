bf77449048cca1e459f4db8b17947ae9eb15871a
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-04 17:22:28 -0600

    change iPython to Jupyter

--- ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-LaunchJupyter.sh
+++ ChicagoBoothML_Helpy/AWS-EMR/BootActs/AWS-EMR-BootAct-LaunchJupyter.sh
@@ -5,7 +5,7 @@
 set -x -e
 
 
-# launch iPython on Master node only
+# launch Jupyter on Master node only
 if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
     # change Home directory
@@ -16,10 +16,9 @@ then
 
     # create iPython profile
     /usr/local/bin/ipython profile create default
-    echo "c = get_config()"                    > $IPYTHON_NOTEBOOK_CONFIG_FILE_PATH
-    echo "c.NotebookApp.ip = '*'"             >> $IPYTHON_NOTEBOOK_CONFIG_FILE_PATH
-    echo "c.NotebookApp.open_browser = False" >> $IPYTHON_NOTEBOOK_CONFIG_FILE_PATH
-    echo "c.NotebookApp.port = 8133"          >> $IPYTHON_NOTEBOOK_CONFIG_FILE_PATH
+
+    # download & override Jupyter Notebook Config file
+    curl $GITHUB_REPO_RAW_PATH/.config/$JUPYTER_NOTEBOOK_CONFIG_FILE_NAME --output $JUPYTER_DIR/$JUPYTER_NOTEBOOK_CONFIG_FILE_NAME
 
     # launch iPython server
     nohup /usr/local/bin/ipython notebook --no-browser > /mnt/var/log/python_notebook.log &
